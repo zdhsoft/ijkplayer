d3594fa99976b8627857e14e37069ba47d218a48
zdhsoft <zdhsoft@163.com>
2018-12-26 18:46:26 +0800

    11

--- ijkmedia/ijkplayer/ff_ffplay.c
+++ ijkmedia/ijkplayer/ff_ffplay.c
@@ -3743,13 +3743,13 @@ static int read_thread(void *arg)
 			if(dsv_param.program_type == DSV_PROGRAM_TYPE_NONE) {
 				av_log(NULL, AV_LOG_ERROR, "*******************avformat_find_stream_info************* <<<<");
 				InitVideoDecoderByDSVParamEx(ic, &dsv_param); //使用预处理的方案
-	            avformat_find_stream_info(ic, opts);  //如果不是预置的，则启动侦测
+	            //avformat_find_stream_info(ic, opts);  //如果不是预置的，则启动侦测
 				av_log(NULL, AV_LOG_ERROR, "*******************avformat_find_stream_info************* >>>>");
 			}
 			else {
 				av_log(NULL, AV_LOG_ERROR, "*******************InitVideoDecoderByDSVParam************* <<<<< %d", dsv_param.program_type);
 				InitVideoDecoderByDSVParamEx(ic, &dsv_param); //使用预处理的方案
-				avformat_find_stream_info(ic, opts);  //如果不是预置的，则启动侦测
+				//avformat_find_stream_info(ic, opts);  //如果不是预置的，则启动侦测
 				av_log(NULL, AV_LOG_ERROR, "*******************InitVideoDecoderByDSVParam************* >>>>> %d", dsv_param.program_type);
 			}
 			
